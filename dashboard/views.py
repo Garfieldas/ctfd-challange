ef1379b0ab3f133ffc7e4dd252166c377c7abf8f
Garieldas <dainiusrainys99@gmail.com>
2025-05-05 23:11:17 +0300

    Added 2 flags

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import TaxDeclaration
 from django.contrib.auth.decorators import login_required
 
+@login_required
 def dashboard(request, pk):
     declaration = TaxDeclaration.objects.get(id=pk)
 
