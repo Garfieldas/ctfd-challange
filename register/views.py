b521e52f938e48cb73e01c3fcf633c675a15c0c8
Garieldas <dainiusrainys99@gmail.com>
2025-05-05 22:31:34 +0300

    Registation added

--- register/views.py
+++ register/views.py
@@ -1,4 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login as auth_login
+from django.contrib import messages
 
 def register_view(request):
-    return render (request, 'register/index.html')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        confirmPassword = request.POST.get('confirm-password')
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Vartotojas su šiuo prisijungimo vardu jau egzistuoja!')
+            return render(request, 'register/index.html')
+
+        if password != confirmPassword:
+            messages.error(request, 'Slaptažodžiai nesutampa!')
+            return render(request, 'register/index.html')
+
+        user = User.objects.create_superuser(
+            username=username,
+            password=password,
+        )
+        auth_login(request, user)
+        return redirect('taxes')
+
+    return render(request, 'register/index.html')
